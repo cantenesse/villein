4e1e4d7803f5de7cbb6fb2a61a1309eaade42e2e
Chris Antenesse <chris@antenesse.net>
2014-04-16 23:46:10 -0700

    cleaned up walking.  matching process name to stuff in /proc

--- villein.go
+++ villein.go
@@ -1,86 +1,115 @@
 package main
 
 import (
-  "path/filepath"
-  "os"
-  "flag"
-  "fmt"
-  _ "regexp"
-  "io/ioutil"
-  "encoding/json"
-  _ "strings"
-  _ "io"
+	"encoding/json"
+	"fmt"
+	_ "io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	_ "strings"
 )
 
 type AppList struct {
-  Applications []App
+	Applications []App
 }
 
 type App struct {
-  Name string
-  StartCommand map[string]string `json:"start_command"`
-  ProcName string `json:"procname"`
+	Name         string
+	StartCommand map[string]string `json:"start_command"`
+	ProcName     string            `json:"procname"`
+}
+
+type WalkError struct {
+	Message string
+}
+
+func (w WalkError) Error() string {
+	return w.Message
 }
 
 func NewAppList(source []byte) *AppList {
-  al := new(AppList)
-  al.Applications = make([]App, 0, 0)
-  al.FromJson(source)
-  return al
+	al := new(AppList)
+	al.Applications = make([]App, 0, 0)
+	al.FromJson(source)
+	return al
 }
 
 func (al *AppList) FromJson(source []byte) {
-  c := make(map[string]App)
+	c := make(map[string]App)
 
-  e := json.Unmarshal(source, &c)
+	e := json.Unmarshal(source, &c)
 
-  if e != nil {
-    fmt.Printf("unable decode json")
-  }
+	if e != nil {
+		fmt.Printf("unable decode json")
+	}
 
-   for key, value := range c {
-    value.Name = key
-    al.Applications = append(al.Applications, value)
-  }
-
+	for key, value := range c {
+		value.Name = key
+		al.Applications = append(al.Applications, value)
+	}
 }
 
-func visit(path string, f os.FileInfo, err error) error {
-  fmt.Printf("Visited: %s\n", path)
-  return nil
-} 
+func walkThing(command string) filepath.WalkFunc {
+	return func(path string, f os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-//func is_pid(directory string) {
-//  r, err := regexp.Compile(`[0-9]+`)
-//  
-//  if err != nil {
-//    fmt.Printf("Problem compiling regexp")
-//  }
-//
-// if r.MatchString(directory) == true {
-//   return true
-// } else {
-//   return false
-// }
-//
+		fmt.Println("Checking", path)
+		if is_pid(path) {
+			fmt.Println("Visited:", path)
+			if is_process_running(command, path) {
+				return WalkError{"foo"}
+			}
+		}
+		return nil
+	}
+}
+
+func is_pid(directory string) bool {
+	r, err := regexp.Compile(`[0-9]+$`)
+
+	if err != nil {
+		fmt.Printf("Problem compiling regexp")
+	}
+
+	if r.MatchString(directory) == true {
+		return true
+	}
+
+	return false
+}
+
+func is_process_running(process, pid string) bool {
+	cmdfile := fmt.Sprintf("%s/cmdline", pid)
+	fmt.Println("Checking", cmdfile)
+	f, ferr := ioutil.ReadFile(cmdfile)
+
+	if ferr != nil {
+		fmt.Println("opening of cmdline file failed")
+		return false
+	}
+
+	return string(f) == process
+}
 
 func main() {
-  // read the json file into a string
-  f, ferr := ioutil.ReadFile("conf/villein.json")
-  if ferr != nil {
-    //CJA: figure out logging
-    fmt.Printf("cannot read config")
-  }
+	procfs := "/tmp/proc"
 
-  ap := NewAppList(f)
-  fmt.Println(ap)
-  //config := string(f)
+	// read the json file into a string
+	f, ferr := ioutil.ReadFile("conf/villein.json")
+	if ferr != nil {
+		//CJA: figure out logging
+		fmt.Printf("cannot read config")
+	}
 
- 
-  // get command line arg
-  flag.Parse()
-  
-  root := flag.Arg(0)
-  err := filepath.Walk(root, visit)
-  fmt.Printf("filepath.Walk() returned %v\n", err)
+	ap := NewAppList(f)
+	fmt.Println(ap)
+
+	// get command line arg
+
+	err := filepath.Walk(procfs, walkThing("/home/melite/melite"))
+	fmt.Printf("filepath.Walk() returned %v\n", err)
 }
